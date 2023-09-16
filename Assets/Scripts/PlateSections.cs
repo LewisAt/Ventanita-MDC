27e6c1bebba5e9a1c22c7bcb32af91ca36cd9b08
GunzRuls <143023790+GunzRuls@users.noreply.github.com>
2023-09-16 14:35:46 -0400

    Finishing food pickup and drop
    Also worked on filling the plate and having a bool to track the plate's progress to being full

---
+++ Assets/Scripts/PlateSections.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateSections : MonoBehaviour
+{
+    public GameObject[] platePositions;
+    //public foodIdentifier foodIdentifier;
+    foodIdentifier.typesOfFood enumholder;
+
+    /*private void OnCollisionEnter(Collision other)
+    {
+        laldletrigger = other.gameObject.GetComponentInParent<LaldleTrigger>();
+
+        if (other.gameObject.tag == "food")
+        {
+            laldletrigger.CheckSpoonForPlate(other.gameObject);
+        }
+    }*/
+
+    /*private void Update()
+    {
+        enumholder =
+            //foodIdentifier.typesOfFood
+
+        for (int i = 0; i < foodIdentifier.food; i++)
+        {
+            if (i == foodIdentifier.typesOfFood.croqueta)
+            {
+
+            }
+        }
+
+    }*/
+}
