f89b2bc2d0b7a18d35096d44d27bca94a213534d
unknown <bryan.estopinan001@mymdc.net>
2023-10-16 12:49:51 -0400

    fixed some things

--- Assets/-Rogers/Rogers Scripts/rogerTimer.cs
+++ Assets/-Rogers/Rogers Scripts/rogerTimer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Timer : MonoBehaviour
+public class rogerTimer : MonoBehaviour
 {
     public int time = 60;
     public Text timeLeft;
