f9819e4962f937ef7c9b0c68a1b2f1fe1d648318
unknown <Joel.Figueroa002@mymdc.net>
2023-10-25 14:07:06 -0400

    Fixed laldle trigger script having a static bool which was causing problems when trying to have several different laldles. Also, added to the script specifications for laldles to only be able to interact with their food type.

---
+++ Assets/-AlphaW4123A/scripts/LaldleIdentifier.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaldleIdentifier : MonoBehaviour
+{
+    public enum TypeOfLaldle
+    {
+        general,
+        tostone,
+        maduro,
+        FraseDePollo,
+        Rabo,
+        Beans
+    }
+
+    public TypeOfLaldle typeOfLaldle;
+}
