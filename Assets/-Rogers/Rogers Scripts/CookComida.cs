b7344e86294fa322bdb7cfba57fec6ed059d410b
Ropbdgert <Alexslander45@gmail.com>
2023-11-15 13:03:44 -0500

    Added scene for cook testing

---
+++ Assets/-Rogers/Rogers Scripts/CookComida.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookComida : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+}
