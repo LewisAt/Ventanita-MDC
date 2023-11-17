301cf978646459a99e7fd4fd398b43433b9b347d
Bryan R <spacedout3313@gmail.com>
2023-11-17 15:37:09 -0500

    Revert "Revert "y""
    This reverts commit 88247cf3d9fee95616c575981c52e4c01388fa20.

--- Assets/StudentsFolder/-Bryan E/B.E Scripts/Movetowindow.cs
+++ Assets/StudentsFolder/-Bryan E/B.E Scripts/Movetowindow.cs
@@ -123,20 +123,11 @@ public class Movetowindow : MonoBehaviour
     }
     private void Update()
     {
-        if (check2 == true && check3 == false)
+       if(check2 == true && check3 == false)
         {
             tipReduce -= 0.0133f * Time.deltaTime;
-            tip = CustomerOrder.foodsCostForCustomer * tipReduce;
         }
         
-        /*
-         // For testing purposes
-        if(Input.GetKeyDown("space"))
-        {
-            CompleteCustomerCorrect();
-        }
-        */
-
     }
     void SpawnCustomer()
     {
