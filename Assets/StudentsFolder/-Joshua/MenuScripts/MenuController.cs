8c770a44cbf5ca963fc8ed52f555fc07bcc24d47
unknown <Joshua.Bordas001@mymdc.net>
2024-01-08 14:09:11 -0500

    Added more comments to code

--- Assets/StudentsFolder/-Joshua/MenuScripts/MenuController.cs
+++ Assets/StudentsFolder/-Joshua/MenuScripts/MenuController.cs
@@ -5,13 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    //Used to start the game when the proper button is pressed
     public void StartBtn()
     {
+        //Loads the scene that matches with the scene number referenced in the script
         SceneManager.LoadScene(1);
     }
 
+    //Used to send the player to the starting menu
     public void Back2Menu()
     {
+        //Loads the scene that has the same name as the scene referenced in script
         SceneManager.LoadScene("StartingMenuScene");
     }
 }
