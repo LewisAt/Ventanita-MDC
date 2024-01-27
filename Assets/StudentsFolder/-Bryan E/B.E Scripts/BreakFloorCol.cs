4fcfbe66204fb8c30fc00f18074f2f311915ad94
unknown <bryanandhismind@gmail.com>
2024-01-27 02:34:56 -0500

    Added sound to plate break

--- Assets/StudentsFolder/-Bryan E/B.E Scripts/BreakFloorCol.cs
+++ Assets/StudentsFolder/-Bryan E/B.E Scripts/BreakFloorCol.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class BreakFloorCol : MonoBehaviour
 {
+    public AudioSource breaksound;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "plate")
         {
             Debug.Log("Hello floor");
+            breaksound.Play();
             StartCoroutine(other.gameObject.GetComponent<PlateBreak>().startBreak());
         }
     }
