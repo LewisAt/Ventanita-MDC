30f91c040b7e0a79aaf52088adf1dc12aac01c02
unknown <bryanandhismind@gmail.com>
2024-01-31 18:15:46 -0500

    Fixed bugs

--- Assets/StudentsFolder/-Bryan E/B.E Scripts/ReleaseDistance.cs
+++ Assets/StudentsFolder/-Bryan E/B.E Scripts/ReleaseDistance.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ReleaseDistance : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Transform player;
     void Update()
     {
-        
+        if(Vector3.Distance(player.position,transform.position) > 0.4)
+        {
+            
+        }
     }
 }
