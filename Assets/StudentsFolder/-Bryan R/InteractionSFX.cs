b9d59a63039726a1076b59b51c716f3ae82f5040
Bryan R <spacedout3313@gmail.com>
2024-02-02 18:26:18 -0500

    in process of testing SFX... please wait.

---
+++ Assets/StudentsFolder/-Bryan R/InteractionSFX.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionSFX : MonoBehaviour
+{
+    public AudioSource pickupSFX;
+    public AudioSource dropSFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
