c3bbc68a1d5410c9aa15ed041c6eab2349c09552
Bryan R <spacedout3313@gmail.com>
2024-03-04 12:44:55 -0500

    finishing up Smooth-Snap turning

--- Assets/StudentsFolder/-Bryan R/TurnSwitcher.cs
+++ Assets/StudentsFolder/-Bryan R/TurnSwitcher.cs
@@ -1,42 +1,90 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class TurnSwitcher : MonoBehaviour
 {
-    public GameObject playerXROrigin;
-
+    public GameObject playerHand;   // Left or Right Hand
+    
     // If bool is True then Smooth turn || false then Snap turn
-    private static bool turnSwitch;
+    private bool turnSwitch = false;
 
+    private GameObject TurnController;
+    public static TurnSwitcher instance;
 
-    void Update()
+    void Awake()
     {
-        if (turnSwitch == true)
+        if (instance == null)
         {
-            playerXROrigin.GetComponent<ActionBasedSnapTurnProvider>().enabled = false;
-            playerXROrigin.GetComponent<ActionBasedContinuousMoveProvider>().enabled = true;
+            instance = this;
         }
-        else if (turnSwitch == false)
+        else
         {
-            playerXROrigin.GetComponent<ActionBasedSnapTurnProvider>().enabled = true;
-            playerXROrigin.GetComponent<ActionBasedContinuousMoveProvider>().enabled = false;
+            Destroy(this.gameObject);
         }
+        DontDestroyOnLoad(this.gameObject);
     }
 
     public void SmoothTurnAllowed()
     {
         turnSwitch = true;
-        /*playerXROrigin.GetComponent<ActionBasedSnapTurnProvider>().enabled = false;
-        playerXROrigin.GetComponent<ActionBasedContinuousMoveProvider>().enabled = true;*/
     }
 
     public void SnapTurnAllowed()
     {
         turnSwitch = false;
-        /*playerXROrigin.GetComponent<ActionBasedSnapTurnProvider>().enabled = true;
-        playerXROrigin.GetComponent<ActionBasedContinuousMoveProvider>().enabled = false;*/
+    }
+
+    /*void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // called second
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        SceneInitialSetup();
+        Debug.Log("general manager has been started and the scene has been setup");
+
+    }*/
+
+    private GameObject Turncontroller;
+    void findTurnController()
+    {
+        TurnController = GameObject.Find("XR Player Rig/XR Origin (XR Rig)/Locomotion System/Turn");
+        if (TurnController == null)
+        {
+            Debug.Log("TurnController has not been found");
+        }
+        else
+        {
+            Debug.Log("TurnController has been found");
+        }
+    }
+
+    public void SetTurningOption(bool SmoothTurning)
+    {
+        turnSwitch = SmoothTurning;
+        Debug.Log("SmoothTurning has been toggled and the value is now: " + turnSwitch);
+        if (TurnController == null)
+        {
+            findTurnController();
+        }
+
+        if (turnSwitch == true)
+        {
+            playerHand.GetComponent<ActionBasedControllerManager>().smoothTurnEnabled = true;
+        }
+        else if (turnSwitch == false)
+        {
+            playerHand.GetComponent<ActionBasedControllerManager>().smoothTurnEnabled = false;
+        }
+    }
+
+    void SceneInitialSetup()
+    {
+        findTurnController();
+        SetTurningOption(turnSwitch);
     }
 }
