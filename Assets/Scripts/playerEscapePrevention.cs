eaeaed69e034d6782b3213efdceb93d155684f51
Lewis Tamayo <144160860+LewisAt@users.noreply.github.com>
2024-04-09 13:41:24 -0400

    long ago

--- Assets/Scripts/playerEscapePrevention.cs
+++ Assets/Scripts/playerEscapePrevention.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class playerEscapePrevention : MonoBehaviour
 {
+    private GameObject Player;
+    private Vector3 RigLocation;
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Was inside at start");
+            Player = GameObject.FindGameObjectWithTag("Player");
+            RigLocation = Player.transform.parent.transform.position;
+
+        }
+    
+    }
+    
     void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Player is trying to escape");
-            other.gameObject.transform.position = this.transform.position;
+            RigLocation = Player.transform.parent.transform.position;
+            Player.transform.position = RigLocation;
         }
     }
+    void Update()
+    {
+        if (Player == null)
+        {
+            Debug.Log("playerWasOutsideOnstart");
+            Player.transform.position = RigLocation;
+        }
+    }
+    
+
 }
